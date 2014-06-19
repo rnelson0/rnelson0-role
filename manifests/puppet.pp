b99655055a6760d2a8500503f25ca7d742e343b8
Rob Nelson <rnelson0@gmail.com>
2014-06-19 16:07:25 +0000

    Add puppet role

---
+++ manifests/puppet.pp
@@ -0,0 +1,32 @@
+# == Class: role::puppet
+#
+# Puppet Master role
+#
+# === Parameters
+#
+# None
+#
+# === Variables
+#
+# None
+#
+# === Examples
+#
+#  include role::puppet
+#
+# === Authors
+#
+# Rob Nelson <rnelson0@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2014 Rob Nelson
+#
+class role::puppet {
+  include profile::base  # All roles should have the base profile
+  include profile::puppet_master
+  include profile::puppetdb
+  include profile::hiera
+  include profile::mcollective::users
+  include profile::mcollective::all
+}
