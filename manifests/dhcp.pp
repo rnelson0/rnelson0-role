882065f5ac6f1a8fb562e30a52c8bbe6ed12ea0e
Rob Nelson <rnelson0@gmail.com>
2014-06-19 17:34:04 +0000

    Add role::dhcp for dhcp servers

---
+++ manifests/dhcp.pp
@@ -0,0 +1,30 @@
+# == Class: role::dhcp
+#
+# DHCP Server role
+#
+# === Parameters
+#
+# None
+#
+# === Variables
+#
+# None
+#
+# === Examples
+#
+#  include role::dhcp
+#
+# === Authors
+#
+# Rob Nelson <rnelson0@gmail.com>
+#
+# === Copyright
+#
+# Copyright 2014 Rob Nelson
+#
+class role::dhcp {
+  include profile::base  # All roles should have the base profile
+  include profile::dhcp
+
+  create_resources(::dhcp::server::subnet, hiera_hash('dhcp_subnet'))
+}
